a51b43ad32a005ebe9698355a537bfe97fda0a47
JardelFerreira <jardelferreira.sousa@gmail.com>
2026-04-14 11:46:13 -0300

    Ajustes para o ACL providers e grupos de rotas

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,23 +1,53 @@
 <?php
 
 use App\Http\Middleware\HandleInertiaRequests;
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets;
+use Illuminate\Support\Facades\Request;
+use Illuminate\Support\Facades\Route;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
+        then: function () {
+            Route::middleware('web') // ou 'api'
+                // ->prefix('custom') // opcional, prefixa as URLs
+                // ->group(base_path('routes/tenants.php'));
+                // função que carrega todos as rotas da pasta
+                ->group(function () {
+                    foreach (glob(base_path('routes/*.php')) as $routeFile) {
+                        require $routeFile;
+                    }
+                });
+        },
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->web(append: [
             HandleInertiaRequests::class,
             AddLinkHeadersForPreloadedAssets::class,
         ]);
+        $middleware->alias([
+            'role' => \Spatie\Permission\Middleware\RoleMiddleware::class,
+            'permission' => \Spatie\Permission\Middleware\PermissionMiddleware::class,
+            'role_or_permission' => \Spatie\Permission\Middleware\RoleOrPermissionMiddleware::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
+        $exceptions->render(function (
+            AuthenticationException $e,
+            Request $request
+        ) {
+            $guard = $e->guards()[0] ?? null;
+            // Tenant
+            // if ($guard === 'tenant') {
+            //     return redirect()->guest(route('tenant.login'));
+            // }
+            // Admin
+            return redirect()->guest(route('login'));
+        });
     })->create();
